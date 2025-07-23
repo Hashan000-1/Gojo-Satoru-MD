8af7f1f4e4239f70a4805ca3af57a034dc968df3
Hashan000-1 <pabasarahashan977@gmail.com>
2025-07-23 17:28:34 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,9 +5,9 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "false",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/h2vC7XG/Gojo-satoru-md-bot-2.jpg",
-SESSION_ID: process.env.SESSION_ID || "PZszSBhY#89k3z1R8aIG49xm0K8jOSs8D1e7FApgAPor0mdLaQ-c",
+SESSION_ID: process.env.SESSION_ID || "k8QzkJCA#Aigu_ybSZBObQF_a9yzcRYhR0ibbDAIBGv-7aih21L8",
 };
 
 
